eec7aa393321e77d5d3bdf018d38598b5848236d
JavaCommons Technologies <javacommons@gmail.com>
2024-08-14 10:03:59 +0900

    .

--- cs-tool/build.sh
+++ cs-tool/build.sh
@@ -7,4 +7,5 @@ name=tool
 rm -rf bin obj
 dotnet build -c Release
 cd bin/x64/Release/net462
-ilmerge //wildcards //out:`cygpath -w $cwd/$name.exe` $name.exe *.dll
+#ilmerge //wildcards //out:`cygpath -w $cwd/$name.exe` $name.exe *.dll
+ilmerge //wildcards //out:`cygpath -w $HOME/cmd/$name.exe` $name.exe *.dll
