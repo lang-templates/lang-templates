b31ac98deadb8432abc1791ed33d6154ce772b62
JavaCommons Technologies <javacommons@gmail.com>
2024-08-14 09:57:15 +0900

    .

--- cpp-tool/build.sh
+++ cpp-tool/build.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env bash
+set -uvx
+set -e
+cd "$(dirname "$0")"
 cwd=`pwd`
+name=tool
 rm -rf bin obj
 dotnet build -c Release
 cd bin/x64/Release/net462
-ilmerge //wildcards //out:`cygpath -w $cwd/lt.exe` lt.exe *.dll
+ilmerge //wildcards //out:`cygpath -w $cwd/$name.exe` $name.exe *.dll
