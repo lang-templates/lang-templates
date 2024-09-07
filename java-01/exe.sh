d341f68527cf7d22453d8cfa11063a6f8d9214e2
JavaCommons Technologies <javacommons@gmail.com>
2024-09-07 18:54:36 +0900

    .

--- java-01/exe.sh
+++ java-01/exe.sh
@@ -10,28 +10,4 @@ rm -rf win64.tmp
 rm -rf tmp.dist
 mkdir tmp.dist
 exewrap64 -o tmp.dist/$name.exe -i duke256.ico -t 17 build/libs/noname-all.jar
-#$JAVA_HOME/bin/jlink --compress=2 --add-modules java.base,java.desktop,java.prefs --output tmp.dist/jre-min
-exewrap64 -o tmp.dist/$name.exe -i duke256.ico -t 17 build/libs/noname-all.jar
-$JAVA_HOME/bin/jlink --compress=2 --add-modules java.base,\
-java.compiler,\
-java.datatransfer,\
-java.desktop,\
-java.instrument,\
-java.logging,\
-java.management,\
-java.management.rmi,\
-java.naming,\
-java.net.http,\
-java.prefs,\
-java.rmi,\
-java.scripting,\
-java.se,\
-java.security.jgss,\
-java.security.sasl,\
-java.smartcardio,\
-java.sql,\
-java.sql.rowset,\
-java.transaction.xa,\
-java.xml,\
-java.xml.crypto \
---output tmp.dist/jre-min
+cp -rp ~/cmd/java/jre-min tmp.dist/jre-min
