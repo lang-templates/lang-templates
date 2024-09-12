a594f94cf6e70a9fbdc1544f3a308dddf71eba12
JavaCommons Technologies <javacommons@gmail.com>
2024-09-12 15:04:26 +0900

    .

--- java-01/src/main/java/demo/WaitUntil.java
+++ java-01/src/main/java/demo/WaitUntil.java
@@ -1,9 +1,10 @@
 package demo;
 
-//import org.joda.time.*;
-//import org.joda.time.format.PeriodFormatter;
-//import org.joda.time.format.PeriodFormatterBuilder;
 import system.Sys;
+
+import java.util.Calendar;
+
+//import java.util.Calendar;
 
 class Sleeper {
     public static void sleep(long millis) {
@@ -13,7 +14,18 @@ class Sleeper {
             ;
         }
     }
-    public static void waitUntil(org.joda.time.DateTime dt) {
+    public static void waitOnConsole(java.util.Date date) {
+        org.joda.time.DateTime dt = new org.joda.time.DateTime(date);
+        waitOnConsole(dt, System.err);
+    }
+    public static void waitOnConsole(java.util.Date date, java.io.PrintStream stream) {
+        org.joda.time.DateTime dt = new org.joda.time.DateTime(date);
+        waitOnConsole(dt, stream);
+    }
+    public static void waitOnConsole(org.joda.time.DateTime dt) {
+        waitOnConsole(dt, System.err);
+    }
+    public static void waitOnConsole(org.joda.time.DateTime dt, java.io.PrintStream stream) {
         var start = new org.joda.time.DateTime();
         var formatter = new org.joda.time.format.PeriodFormatterBuilder()
                 .appendYears()
@@ -43,22 +55,22 @@ class Sleeper {
             sb.append(" left.");
             String s = sb.toString();
             if (!s.equals(msg)) {
-                System.err.print("\r");
-                System.err.print("\033[1F\33[K");
-                System.err.print(s);
-                System.err.flush();
+                stream.print("\r");
+                stream.print("\033[1F\33[K");
+                stream.print(s);
+                stream.flush();
                 msg = s;
             }
             Sleeper.sleep(100);
         }
-        System.err.print("\r");
-        System.err.print("\033[1F\33[K");
-        System.err.print("Waiting...done (");
+        stream.print("\r");
+        stream.print("\033[1F\33[K");
+        stream.print("Waiting...done (");
         var elapsed = new org.joda.time.Interval(start, new org.joda.time.DateTime());
-        System.err.print(elapsed.toPeriod().toString(formatter));
-        System.err.print(").");
-        System.err.println();
-        System.err.flush();
+        stream.print(elapsed.toPeriod().toString(formatter));
+        stream.print(").");
+        stream.println();
+        stream.flush();
         Sleeper.sleep(1000);
     }
 }
@@ -66,11 +78,16 @@ class Sleeper {
 public class WaitUntil {
     public static void main(String[] args) throws Exception {
         Sys.echo("hello");
-        var now = new org.joda.time.DateTime();
-        var dt = now.plusSeconds(15);
+        //var now = new org.joda.time.DateTime();
+        //var dt = now.plusSeconds(15);
+        var now = new java.util.Date();
+        var calendar = java.util.Calendar.getInstance();
+        calendar.setTime(now);
+        calendar.add(Calendar.SECOND, 15);
+        var dt = calendar.getTime();
         Sys.echo(now, "now");
         Sys.echo(dt, "now");
-        Sleeper.waitUntil(dt);
+        Sleeper.waitOnConsole(dt);
         Sys.echo("Finished");
     }
 }
