8b636a02420fcea46848570a2820a086f9c21d3f
JavaCommons Technologies <javacommons@gmail.com>
2024-09-13 08:43:47 +0900

    .

--- java01/exe.sh
+++ java01/exe.sh
@@ -9,5 +9,11 @@ ls -ltr build/libs
 rm -rf win64.tmp
 rm -rf tmp.dist
 mkdir tmp.dist
-exewrap64 -o tmp.dist/$name.exe -i duke256.ico -t 17 -v 1.0.0 build/libs/noname-all.jar
+exewrap64 \
+  -g \
+  -o tmp.dist/$name.exe \
+  -i duke256.ico \
+  -t 17 \
+  -v 1.0.0 \
+  -j build/libs/noname-all.jar
 cp -rp ~/cmd/java/jre-min tmp.dist/jre-min
