30b30d24f7fe28b23a0ead7beeb13694cb31d62a
JavaCommons Technologies <javacommons@gmail.com>
2024-09-13 22:46:42 +0900

    .

--- java01/src/main/java/demo/ORM.java
+++ java01/src/main/java/demo/ORM.java
@@ -1,5 +1,6 @@
 package demo;
 
+import com.j256.ormlite.dao.CloseableWrappedIterable;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.field.DatabaseField;
@@ -7,36 +8,51 @@ import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.DatabaseTable;
 import com.j256.ormlite.table.TableUtils;
+import system.Sys;
+
+import java.sql.SQLException;
 
 public class ORM {
     public static void main(String[] args) throws Exception {
-        // this uses h2 but you can change it to match your database
         //String databaseUrl = "jdbc:h2:mem:account";
         String databaseUrl = "jdbc:sqlite:test.db3";
-       // create a connection source to our database
+        // create a connection source to our database
         ConnectionSource connectionSource =
                 new JdbcConnectionSource(databaseUrl);
 
-       // instantiate the DAO to handle Account with String id
+        // instantiate the DAO to handle Account with String id
         Dao<Account, String> accountDao =
                 DaoManager.createDao(connectionSource, Account.class);
 
-       // if you need to create the 'accounts' table make this call
-        TableUtils.createTable(connectionSource, Account.class);
+        // if you need to create the 'accounts' table make this call
+        TableUtils.createTableIfNotExists(connectionSource, Account.class);
 
-       // create an instance of Account
+        //accountDao.deleteById("Jim Smith");/**/
+        try (CloseableWrappedIterable<Account> iterable
+                     = accountDao.getWrappedIterable()) {
+            iterable.forEach(account -> {
+                Sys.echo(account.getName());
+                try {
+                    accountDao.delete(account);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        // create an instance of Account
         String name = "Jim Smith";
         Account account = new Account(name, "_secret");
 
-       // persist the account object to the database
+        // persist the account object to the database
         accountDao.create(account);
 
-       // retrieve the account
+        // retrieve the account
         Account account2 = accountDao.queryForId(name);
-       // show its password
+        // show its password
         System.out.println("Account: " + account2.getPassword());
 
-       // close the connection source
+        // close the connection source
         connectionSource.close();
     }
 }
@@ -51,6 +67,7 @@ class Account {
 
     Account() {
     }
+
     public Account(String name, String password) {
         this.name = name;
         this.password = password;
@@ -59,12 +76,15 @@ class Account {
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
