d614c2a529fc607b57e8b5448af856833bfafc7c
JavaCommons Technologies <javacommons@gmail.com>
2024-09-15 04:17:28 +0900

    .

---
+++ java01/src/main/java/demo/FormLayoutDemo.java
@@ -0,0 +1,16 @@
+package demo;
+
+import javax.swing.*;
+
+public class FormLayoutDemo extends system.JFrame {
+    private JButton button1;
+    private JPanel panel1;
+    public FormLayoutDemo() {
+        this.setMainPanel(panel1, 640, 480, "FormLayoutDemo");
+    }
+
+    public static void main(String[] args) {
+        var frame = new FormLayoutDemo();
+        frame.setVisible(true);
+    }
+}
