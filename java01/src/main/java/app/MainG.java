01a87ece9eed6389b5b7ba44c26c112cdecac819
JavaCommons Technologies <javacommons@gmail.com>
2024-09-15 03:33:38 +0900

    .

--- java01/src/main/java/app/MainG.java
+++ java01/src/main/java/app/MainG.java
@@ -9,11 +9,12 @@ import java.awt.event.ActionListener;
 public class MainG extends system.JFrame {
     private JPanel mainPanel;
     private JButton showArgsButton;
-    private JLabel labelShowArgs;
     private JButton directoryTreeButton;
     private JButton repoTreeButton;
     private JButton ダイアログを表示Button;
     private JLabel dialogResultLabel;
+    private JButton アイコン表示テストButton;
+    private JLabel ここに引数を表示Label;
 
     public static void main(String[] args) {
         var frame = new MainG();
@@ -24,9 +25,9 @@ public class MainG extends system.JFrame {
         showArgsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                MainG.this.labelShowArgs.setText("クリックされました: [" + Main.argsList + "]");
+                MainG.this.ここに引数を表示Label.setText("クリックされました: [" + Main.argsList + "]");
                 MainG.this.update(MainG.this.getGraphics());
-                Sys.println("button clicked クリック");
+                Sys.println("showArgsButton clicked クリック");
             }
         });
         directoryTreeButton.addActionListener(new ActionListener() {
@@ -50,6 +51,14 @@ public class MainG extends system.JFrame {
                 MainG.this.dialogResultLabel.setText("[" + dialog.result + "]");
                 MainG.this.update(MainG.this.getGraphics());
                 Sys.echo(dialog.result, "dialog.result");
+            }
+        });
+        アイコン表示テストButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                var frame = new demo.GUI01();
+                frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+                frame.setVisible(true);
             }
         });
     }
