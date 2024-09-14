de2b2a3a7512c89729fb50fb93fa307b7ba0646d
JavaCommons Technologies <javacommons@gmail.com>
2024-09-14 20:20:45 +0900

    .

--- java01/src/main/java/system/JDialog.java
+++ java01/src/main/java/system/JDialog.java
@@ -9,8 +9,9 @@ public class JDialog extends javax.swing.JDialog {
     }
     public void setMainPanel(JPanel panel, int width, int height, String title) {
         setContentPane(panel);
+        setModal(true);/**/
         setTitle(title);
-        //setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setResizable(false);
         setSize(width, height);
         setLocationRelativeTo(null);
