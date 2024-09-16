916c0a49482f9e3c99e378857f347d1609db268e
JavaCommons Technologies <javacommons@gmail.com>
2024-09-16 15:14:30 +0900

    .

---
+++ java01/src/main/java/demo/RepoSearch.java
@@ -0,0 +1,59 @@
+package demo;
+
+import org.eclipse.swt.widgets.TreeItem;
+import system.Sys;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import java.io.File;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+public class RepoSearch {
+    public static void main(String[] args) {
+        String dir = "D:\\.repo\\base14";
+        final DefaultMutableTreeNode root = new DefaultMutableTreeNode();
+        addDirectory(root, dir, false);
+        DefaultTreeModel model = new DefaultTreeModel(root);
+        //DefaultMutableTreeNode root = (DefaultMutableTreeNode) model.getRoot();
+
+        // Java 9:
+        Collections.list(root.depthFirstEnumeration()).stream()
+                .filter(DefaultMutableTreeNode.class::isInstance)
+                .map(DefaultMutableTreeNode.class::cast)
+                .dropWhile(DefaultMutableTreeNode::isRoot)
+                //.dropWhile(DefaultMutableTreeNode::isLeaf)
+                .map(DefaultMutableTreeNode::getUserObject)
+                .map(File.class::cast)
+                .filter(x -> x != null)
+                .map(File::getPath)
+                .forEach(System.out::println);
+    }
+
+    private static void addDirectory(DefaultMutableTreeNode parent, String path, boolean isDummy) {
+
+        //! ディレクトリ直下のファイル一覧を取得する.
+        path += "\\";
+        File dir = new File(path);
+        File[] files = dir.listFiles();
+
+        if (files == null) {
+            return;
+        }
+        for (int i = 0; i < files.length; i++) {
+            File file = files[i];
+            if (file.isDirectory() && !file.isHidden() && file.canRead()) {
+                //! サブディレクトリを追加する.
+                var node = new DefaultMutableTreeNode(file);
+                parent.add(node);
+
+                //! サブディレクトリの存在チェックの場合は、1件検出した時点で処理を終了する.
+                if (isDummy) {
+                    return;
+                }
+                //! サブディレクトリが存在するかどうかを調べる.
+                addDirectory(node, file.getPath(), isDummy);
+            }
+        }
+    }
+}
