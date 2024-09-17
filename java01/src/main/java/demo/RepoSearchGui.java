b93bdfeac1df39c581d24496f74bd0121de479e1
JavaCommons Technologies <javacommons@gmail.com>
2024-09-18 01:26:20 +0900

    .

--- java01/src/main/java/demo/RepoSearchGui.java
+++ java01/src/main/java/demo/RepoSearchGui.java
@@ -4,25 +4,47 @@ import common.DirModel;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class RepoSearchGui extends system.JFrame {
-    private JPanel mainPanel;
     private JTextField textField1;
-    private JTable table1;
+    private JPanel tablePanel;
+    private JPanel mainPanel;
 
+    private JTable table;
     private DirModel dirModel = new DirModel("D:\\.repo.\\base14");
 
     public RepoSearchGui() {
         this.setMainPanel(mainPanel, "RepoSearchGui");
         this.textField1.grabFocus();
-        DefaultTableModel model = (DefaultTableModel) table1.getModel();
-        // Create a couple of columns
+        // セル編集不可
+        this.table = new JTable(new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        });
+        DefaultTableModel model = (DefaultTableModel) this.table.getModel();
         model.addColumn("Path");
-        // Append a row
-        //model.addRow(new Object[]{"v1", "v2"});
-        //model.addRow(new Object[]{"v1", "v2"});
+        // テーブルダブルクリック
+        this.table.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent mouseEvent) {
+                JTable table =(JTable) mouseEvent.getSource();
+                Point point = mouseEvent.getPoint();
+                int row = table.rowAtPoint(point);
+                if (mouseEvent.getClickCount() == 2 && table.getSelectedRow() != -1) {
+                    // your valueChanged overridden method
+                    var path = (String) model.getValueAt(row, 0);
+                    JOptionPane.showMessageDialog(RepoSearchGui.this, path);
+                }
+            }
+        });
+        this.tablePanel.add(new JScrollPane(this.table));
+
         textField1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
