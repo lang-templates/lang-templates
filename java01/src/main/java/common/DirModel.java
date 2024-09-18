99488d9e10f346c5b67ac65dec2b702c03d34e7b
JavaCommons Technologies <javacommons@gmail.com>
2024-09-18 18:31:34 +0900

    .

--- java01/src/main/java/common/DirModel.java
+++ java01/src/main/java/common/DirModel.java
@@ -8,10 +8,11 @@ import java.util.List;
 
 public class DirModel extends DefaultTreeModel {
     public DirModel(String dir) {
-        super(new DefaultMutableTreeNode());
+        super(new DefaultMutableTreeNode(new File(dir)));
         var root = (DefaultMutableTreeNode) this.getRoot();
         addDirectory(root, dir);
     }
+
     private static void addDirectory(DefaultMutableTreeNode parent, String path/*, boolean isDummy*/) {
         path += "\\";
         File dir = new File(path);
@@ -28,12 +29,14 @@ public class DirModel extends DefaultTreeModel {
             }
         }
     }
+
     public List<File> getFileList() {
         var list = getPathList(false);
         return list.stream()
                 .map(File::new)
                 .toList();
     }
+
     public List<String> getPathList(boolean forwardSlash) {
         var root = (DefaultMutableTreeNode) this.getRoot();
         return Collections.list(root.depthFirstEnumeration()).stream()
@@ -47,7 +50,32 @@ public class DirModel extends DefaultTreeModel {
                 .sorted()
                 .toList();
     }
+
+    public List<String> getPathListLevel1(boolean forwardSlash) {
+        var root = (DefaultMutableTreeNode) this.getRoot();
+        return Collections.list(root.breadthFirstEnumeration()).stream()
+                .filter(DefaultMutableTreeNode.class::isInstance)
+                .map(DefaultMutableTreeNode.class::cast)
+                .takeWhile(node -> node.getLevel() <= 1)
+                .map(DefaultMutableTreeNode::getUserObject)
+                .filter(x -> x != null) // exclude root
+                .map(File.class::cast)
+                .map(File::getPath)
+                .map(x -> forwardSlash ? x.replaceAll("\\\\", "/") : x)
+                .sorted()
+                .toList();
+    }
+
     public List<String> filterByRegex(String regex) {
         return StringListFilter.filterByRegex(getPathList(true), regex);
+    }
+
+    public static void main(String[] args) {
+        var dirModel = new DirModel("D:\\.repo\\base14\\cmd");
+        var list = dirModel.getPathListLevel1(true);
+        list.stream()
+                .forEach( x -> {
+                    System.out.println(x);
+        });
     }
 }
