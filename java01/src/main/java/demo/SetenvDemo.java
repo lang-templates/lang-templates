f0258324b65243136d146e89fba965e2e2cdd40b
JavaCommons Technologies <javacommons@gmail.com>
2024-09-19 06:23:24 +0900

    .

--- java01/src/main/java/demo/SetenvDemo.java
+++ java01/src/main/java/demo/SetenvDemo.java
@@ -6,6 +6,8 @@ public class SetenvDemo {
     public static void main(String[] args) {
         var jver = System.getProperty("java.version");
         Sys.echo(jver);
+        int jver2 = Sys.getJavaVersionAsInt();
+        Sys.echo(jver2, "jver2");
         Sys.setenv("AAA", "aaa");
         Sys.echo(System.getenv("AAA"));
     }
