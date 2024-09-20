39b60705875b40afda40bff2c097992b2e90d519
JavaCommons Technologies <javacommons@gmail.com>
2024-09-20 14:49:48 +0900

    .

---
+++ java01/src/main/java/demo/GetDriveLetter.java
@@ -0,0 +1,13 @@
+package demo;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class GetDriveLetter {
+    public static void main(String[] args) {
+        String dllPathString = "C:\\Program Files\\Notepad++\\scilexer.dll";
+        Path dllPath = Paths.get(dllPathString);
+        Path driveLetter = dllPath.getRoot();
+        System.out.println("The dll is in: " + driveLetter);
+    }
+}
