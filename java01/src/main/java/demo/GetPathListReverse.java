96bc612d8bb7007a823df38c59b3218e6171b10f
JavaCommons Technologies <javacommons@gmail.com>
2024-09-23 00:40:52 +0900

    .

--- java01/src/main/java/demo/GetPathListReverse.java
+++ java01/src/main/java/demo/GetPathListReverse.java
@@ -2,10 +2,17 @@ package demo;
 
 import common.DirModel;
 
+import java.util.Collections;
+
 public class GetPathListReverse {
     public static void main(String[] args) {
         var dirModel = new DirModel("C:\\Program Files\\Zulu\\zulu-11");
-        var list = dirModel.getPathListReverse(true);
+        var list = dirModel.getPathList(true, path -> {
+            if (path.contains("/demo/")) return false;
+            if (path.contains("/legal/")) return false;
+            return true;
+        });
+        Collections.shuffle(list);
         list.stream()
                 .forEach(System.out::println);
     }
