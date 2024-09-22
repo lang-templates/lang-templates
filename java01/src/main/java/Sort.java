0ab9be85303b890f8c63155113957114948d1bb1
JavaCommons Technologies <javacommons@gmail.com>
2024-09-23 05:28:56 +0900

    .

---
+++ java01/src/main/java/Sort.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Sort {
+    public static void main(String[] args) throws Exception {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            int[] number = new int[5];
+            for (int i = 0; i < number.length; i++) {
+                try {
+                    System.out.print("" + (i + 1) + "番目の整数: ");
+                    number[i] = Integer.parseInt(reader.readLine());
+                } catch (Exception ignored) {
+                    System.out.println("" + (i + 1) + "番目の入力に誤りがあります");
+                    i--;
+                }
+            }
+            Arrays.sort(number);
+            for(int Max: number) {
+                System.out.println(Max);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
