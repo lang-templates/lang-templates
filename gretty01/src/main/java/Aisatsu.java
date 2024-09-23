ec221709ad8ec27caecc9681f030974ca279509e
JavaCommons Technologies <javacommons@gmail.com>
2024-09-24 01:35:17 +0900

    .

--- gretty01/src/main/java/Aisatsu.java
+++ gretty01/src/main/java/Aisatsu.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,22 +9,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Aisatsu")
 public class Aisatsu extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response)
-            throws ServletException ,IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.sendRedirect("Aisatsu.html");
     }
 
     @Override
-    protected void doPost(HttpServletRequest request,
-                          HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
         out.println("<html><head></head><body>");
         request.setCharacterEncoding("UTF-8");
         String family = request.getParameter("family");
-        String given  = request.getParameter("given");
+        String given = request.getParameter("given");
         out.printf("こんにちは, %s %s!%n", family, given);
         out.println("</body></html>");
         out.close();
