b26639f68ed6586956e2d196f78824b504da8875
JavaCommons Technologies <javacommons@gmail.com>
2024-09-23 19:57:26 +0900

    .

--- java01/src/main/java/Main.java
+++ java01/src/main/java/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(processInt(10));
-    }
-
-    public static int processInt(int num) {
-        switch (num) {
-            case 10:
-                return num * 10;
-        }
-        return -1;
+        System.out.println("C");
     }
 }
